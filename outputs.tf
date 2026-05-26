5811df845f185d1e62d8569436a9d6710b793037
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-26 23:25:49 +0100

    feat: implement tf-atom-autoscaling-group-aws module (#1)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the Auto Scaling group"
+  value       = try(aws_autoscaling_group.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the Auto Scaling group"
+  value       = try(aws_autoscaling_group.this[0].arn, null)
+}
+
+output "name" {
+  description = "Name of the Auto Scaling group"
+  value       = try(aws_autoscaling_group.this[0].name, null)
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,81 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "min_size" {
+  description = "Minimum number of instances"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of instances"
+  type        = number
+  default     = 3
+}
+
+variable "desired_capacity" {
+  description = "Desired number of instances"
+  type        = number
+  default     = 2
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the ASG"
+  type        = list(string)
+  validation {
+    condition     = length(var.subnet_ids) > 0
+    error_message = "subnet_ids must contain at least one subnet."
+  }
+}
+
+variable "launch_template_id" {
+  description = "ID of the launch template"
+  type        = string
+  validation {
+    condition     = length(var.launch_template_id) > 0
+    error_message = "launch_template_id must not be empty."
+  }
+}
+
+variable "launch_template_version" {
+  description = "Version of the launch template"
+  type        = string
+  default     = "$Latest"
+}
+
+variable "health_check_type" {
+  description = "Health check type (EC2 or ELB)"
+  type        = string
+  default     = "EC2"
+  validation {
+    condition     = contains(["EC2", "ELB"], var.health_check_type)
+    error_message = "health_check_type must be EC2 or ELB."
+  }
+}
+
+variable "health_check_grace_period" {
+  description = "Grace period before health checks start (seconds)"
+  type        = number
+  default     = 300
+}
+
+variable "target_group_arns" {
+  description = "List of target group ARNs to attach"
+  type        = list(string)
+  default     = []
+}
+
+variable "force_delete" {
+  description = "Whether to force-delete the ASG"
+  type        = bool
+  default     = false
+}
+
+variable "instance_refresh_enabled" {
+  description = "Whether to enable instance refresh on updates"
+  type        = bool
+  default     = true
+}
+
+variable "min_healthy_percentage" {
+  description = "Minimum healthy percentage during instance refresh"
+  type        = number
+  default     = 90
+}
